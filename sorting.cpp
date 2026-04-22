795fbed78f8a09d86d46c0732fb8c1384c86a48e
PawanMohit16 <pawanmohit418@gmail.com>
2026-04-22 09:24:09 +0530

    Added issue

--- sorting.cpp
+++ sorting.cpp
@@ -6,7 +6,9 @@ void bubbleSort(int arr[], int n) {
     for(int i = 0; i < n-1; i++) {
         for(int j = 0; j < n-i-1; j++) {
             if(arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
@@ -19,7 +21,7 @@ int main() {
     bubbleSort(arr, n);
 
     cout << "Sorted array: ";
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++);   // ❌ MISTAKE: extra semicolon
         cout << arr[i] << " ";
 
     return 0;
